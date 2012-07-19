cf77b0a9bccc038183aeab29ab364559c35b0ab0
Sam Jarman <sam@samjarman.co.nz>
2012-07-19 20:14:28 +1200

    Second commit, remaining files

---
+++ UCan Browse/popup.js
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+  <head>
+    <title>UCan Browse - An unoffical extension by Sam Jarman</title>
+    <style>
+      body {
+        min-width:275px;
+        overflow-x:hidden;
+		font-family: Verdana, Sans-serif;
+	      }
+	  a{
+		  color:blue;
+		  text-decoration:none;}
+	a:hover{
+		text-decoration:underline;}
+
+    </style>
+    
+   
+	<!--<div style="float:left;"><img src="Icon.png"/> </div> -->
+	<div> <p><b><big>UCan Browse</big></b></p></div>
+    <p><small><i>All links open new in a new tab</i></small></p>
+    
+    <a href="http://www.learn.canterbury.ac.nz/" target="_blank" >Learn</a>
+	</br>
+    <a href="https://mytimetable.canterbury.ac.nz/aplus/apstudent" target="_blank" >My Timetable</a>
+	</br>
+    <a href="http://outlook.com/uclive.ac.nz" target="_blank"  >Student email</a>
+    </br>
+	<a href="https://ucstudentweb.canterbury.ac.nz/?source=currentstudents" target="_blank" >UC Student Web</a>
+    </br>
+    <a href="https://password.canterbury.ac.nz" target="_blank" >Password Reset Tool</a>
+    </br>
+    <a href="http://skydrive.live.com" target="_blank" >My SkyDrive</a>
+    </br>
+    <a href="https://ras.canterbury.ac.nz/" target="_blank" >My IT Account</a>
+    </br>
+    <a href="http://library.canterbury.ac.nz/" target="_blank" >Library</a>
+    </br>
+	<a href="http://www.careerhub.canterbury.ac.nz/Login.chpx?ReturnUrl=/default.chpx" target="_blank" >UC CareerHub</a>
+	</br>
+	<a href="http://ucgoogle1.canterbury.ac.nz/search" target="_blank" >Search UC</a>
+
+	
+	<p><i><small>Created and maintained by
+	    <a href="http://twitter.com/samjarman" target="_blank" >Sam Jarman</a>
+	    </br>
+	    Disclaimer: I do not ask for passwords or any personal information for use in this extension.
+	    </br>
+	    
+	    <a href="mailto:saj77@uclive.ac.nz?Subject=UCan%20Browse"> Send Feedback</a>
+	</small></i></p>
+
+
+    <!-- JavaScript and HTML must be in separate files for security. -->
+   <!--- <script src="popup.js"></script> not needed right now -->
+    
+  </head>
+  <body>
+  </body>
+</html>
